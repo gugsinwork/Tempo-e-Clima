87122e05a5f5cc81daced51249d24a3e8307307b
gugs(Gustavo.L) <123033640+gugsinwork@users.noreply.github.com>
2023-10-26 15:42:52 -0300

    agora foi

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+function buscarPrevisaoTempo() {
+    const cidade = document.getElementById('cidade').value;
+    const apiKey = '6be5f6e0361b740c3d284ade7979ccf2'; 
 
-    document.addEventListener('DOMContentLoaded', function () {
-   
-    const expandButton = document.getElementById('submitButton');
-    const menu = document.getElementById('menu');
-
-
-    expandButton.addEventListener('click', function () {
-        if (menu.style.height === '400px') {
-            menu.style.height = '80px';
-        } else {
-            menu.style.height = '400px';
+    if (!cidade) {
+        alert('Por favor, digite o nome da cidade.');
+        return;
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const previsao = document.getElementById('previsao');
+            previsao.innerHTML = `Cidade: ${data.name}<br><br>`;
+            previsao.innerHTML += `Temperatura: ${data.main.temp}°C<br><br>`;
+            previsao.innerHTML += `Clima: ${data.weather[0].description}<br><br>`;
+            // Adicionar o ícone da condição climática
+            const iconElement = document.createElement('i');
+            iconElement.className = `fas fa-${data.weather[0].icon}`;
+            previsao.appendChild(iconElement);
+        })
+        .catch(error => {
+            alert('Erro ao consultar a previsão do tempo.')});
         }
-    });
-});
-
-
-
-
